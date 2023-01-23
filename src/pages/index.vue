d96295c8261d5268eddcf5dcac361c4afe3166fb
jamesulip <ulip.james@gmail.com>
2023-01-24 03:25:42 +0800

    code fix responsive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 const images = Array.from({ length: 41 }, (_, i) => `/gallery/img${i}.jpg`)
 // check window size responsive
-const responsive = ref(window.innerWidth)
-
+const responsive = ref(0)
+// get window size without using window
+const section = ref(null)
+useResizeObserver(section, (entries) => {
+  const entry = entries[0]
+  const { width } = entry.contentRect
+  responsive.value = width
+})
 const getDeviceType = computed(() => {
   if (responsive.value < 640)
     return 1
@@ -17,21 +23,20 @@ const getDeviceType = computed(() => {
   else
     return 6
 })
-window.addEventListener('resize', () => {
-  responsive.value = window.innerWidth
-})
 
-const randomImagesChunks = images.reduce((acc, _, i) => {
-  const chunkIndex = Math.floor(i / Math.ceil(images.length / getDeviceType?.value))
-  if (!acc[chunkIndex])
-    acc[chunkIndex] = []
-  acc[chunkIndex].push(images[i])
-  return acc
-}, [])
+const randomImagesChunks = computed(() => {
+  return images.reduce((acc, _, i) => {
+    const chunkIndex = Math.floor(i / Math.ceil(images.length / getDeviceType.value))
+    if (!acc[chunkIndex])
+      acc[chunkIndex] = []
+    acc[chunkIndex].push(images[i])
+    return acc
+  }, [])
+})
 </script>
 
 <template>
-  <section class="overflow-hidden text-gray-700 ">
+  <section ref="section" class="overflow-hidden text-gray-700 ">
     <hr class="border-[#25947a] mt-10 border-3">
     <div class="w-full text-center p-4">
       <span class="text-[#25947a] text-3xl font-bold">Gallery</span>
