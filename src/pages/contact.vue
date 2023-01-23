73ef07a2b9f31a4c3a13425f4a227244c1250c74
jamesulip <ulip.james@gmail.com>
2023-01-23 10:39:43 +0800

    added netlify form

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -14,26 +14,28 @@ const form = ref({
     </div>
 
     <!-- create form for name email and message -->
-    <div class="flex flex-col gap-3 p-4 max-w-md mx-auto">
-      <div class="flex flex-col gap-3">
-        <label for="name" class="text-[#25947a] font-bold">Name</label>
-        <input id="name" v-model="form.name" type="text" class="border-[#25947a] border-1 p-2">
-      </div>
-      <div class="flex flex-col gap-3">
-        <label for="email" class="text-[#25947a] font-bold">Email</label>
-        <input id="email" v-model="form.email" type="email" class="border-[#25947a] border-1 p-2">
-      </div>
-      <div class="flex flex-col gap-3">
-        <label for="message" class="text-[#25947a] font-bold">Message</label>
-        <textarea id="message" v-model="form.message" class="border-[#25947a] border-1 p-2" />
-      </div>
+    <form name="contact" netlify>
+      <div class="flex flex-col gap-3 p-4 max-w-md mx-auto">
+        <div class="flex flex-col gap-3">
+          <label for="name" class="text-[#25947a] font-bold">Name</label>
+          <input id="name" v-model="form.name" type="text" class="border-[#25947a] border-1 p-2">
+        </div>
+        <div class="flex flex-col gap-3">
+          <label for="email" class="text-[#25947a] font-bold">Email</label>
+          <input id="email" v-model="form.email" type="email" class="border-[#25947a] border-1 p-2">
+        </div>
+        <div class="flex flex-col gap-3">
+          <label for="message" class="text-[#25947a] font-bold">Message</label>
+          <textarea id="message" v-model="form.message" class="border-[#25947a] border-1 p-2" />
+        </div>
 
-      <div class="flex flex-col gap-3">
-        <button disabled class="bg-[#25947a] text-white p-2 disabled:bg-[#25947a] disabled:opacity-65 disabled:cursor-no-drop">
-          Send
-        </button>
+        <div class="flex flex-col gap-3">
+          <button class="bg-[#25947a] text-white p-2 disabled:bg-[#25947a] disabled:opacity-65 disabled:cursor-no-drop">
+            Send
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   </section>
 </template>
 
