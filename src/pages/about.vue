f44d26e625fc0891f45907b09afa4da43c571128
jamesulip <ulip.james@gmail.com>
2023-01-27 19:09:09 +0800

    code fix fixed about me

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -29,12 +29,7 @@ useHead({
         </div>
         <div class="leading-tight">
           <p class="text-justify">
-            Julie Ann Santos Ulip is a highly skilled and accomplished freelance artist who has been honing her craft from a young age. She was discovered by her art teacher at the age of 10, and with the support of her family and guidance from her teacher, she went on to pursue a degree in Fine Arts with a major in Advertising Design at Far Eastern University. She graduated Magna Cum Laude and decided to continue her career as a freelancer, exploring various branches of visual arts such as tattooing, sculpting, and animation. Her focus has now centered around digital illustrations, particularly portraits. In her free time, she continues to work on her personal paintings and other crafts, always striving to improve her skills.
-          </p>
-          <br>
-          <p class="text-justify">
-            Julie Ann Santos Ulip
-            She decided to start as a freelancer after graduating from college. She took Bachelor of FIne ARts major in Advertsing Design in Far Eastern University and graduated as Magna Cum Laude. She admits that Advertising is not her niche and wanted to explore other branches of visual arts like tattooing, sculpting and animation. Over time, her focus became more and more centered around digital illustrations, especially portraits. During her free time, she prioritized working on her personal paintings and other crafts.
+            I am Julie Ann Santos Ulip, a highly skilled and accomplished freelance artist. I have been honing my craft from a young age and was discovered by my art teacher at the age of 10. With the support of my family and guidance from my teacher, I went on to pursue a degree in Fine Arts with a major in Advertising Design at Far Eastern University. I graduated Magna Cum Laude and decided to continue my career as a freelancer, exploring various branches of visual arts such as tattooing, sculpting, and animation. My focus has now centered around digital illustrations, particularly portraits. In my free time, I continue to work on my personal paintings and other crafts, always striving to improve my skills.
           </p>
         </div>
       </div>
